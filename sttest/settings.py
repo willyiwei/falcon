da8f2d4f23832107e9654d7cc4bcca6629fff785
drinkey <drink3y@gmail.com>
2015-03-10 11:34:59 +0800

    Enable random UserAgent middleware, the list is very long, I might  chose some of them later

--- sttest/settings.py
+++ sttest/settings.py
@@ -15,3 +15,37 @@ NEWSPIDER_MODULE = 'sttest.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'sttest (+http://www.yourdomain.com)'
+
+###############################################################################
+
+#
+#  Followings are add by Junkai, control settings by ourself
+#
+
+CONCURRENT_REQUESTS = 20
+# LOG_LEVEL = 'INFO'
+RETRY_ENABLED = False
+DOWNLOAD_TIMEOUT = 200
+REDIRECT_ENABLED = True
+AJAXCRAWL_ENABLED = True
+
+
+#
+# Enable random UserAgent
+#
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' :None,
+    'sttest.middleware.rotate_useragent.RotateUserAgentMiddleware' :400
+}
+
+ITEM_PIPELINES = {
+    'sttest.pipelines.JsonWriterPipeline': 800,
+}
+
+# AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_START_DELAY
+# AUTOTHROTTLE_MAX_DELAY = 0.5
+# AUTOTHROTTLE_DEBUG
+# CONCURRENT_REQUESTS_PER_DOMAIN
+# CONCURRENT_REQUESTS_PER_IP
+# DOWNLOAD_DELAY
